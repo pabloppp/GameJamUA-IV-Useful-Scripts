41befa6bdbca34f0f4ab0d6931f63f1983975251
Roxanne Lopez van Dooren <roxannelopez6@hotmail.com>
2014-07-02 20:51:26 +0200

    Modified script of Health_System. Added 2 new methods: recieveDmg and recieveHth

--- Assets/scripts/Health_System.cs
+++ Assets/scripts/Health_System.cs
@@ -1,7 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+
+//Script para controlar el sistema de stats de un objeto
+
+public class Health_System : MonoBehaviour {
+
+	public float maxHealth = 100;
+	private float currentHealth = 100;
+	public float armor = 20;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -9,7 +18,24 @@ public class NewBehaviourScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {	
+
 	
 	}
+
+	public void recieveDmg(float dmg){
+		currentHealth -= dmg;
+		if(currentHealth <= 0){
+			currentHealth = 0;
+			gameObject.SendMessage("die");
+		}
+	}
+
+	public void recieveHth(float hth){
+		currentHealth += hth;
+		if(currentHealth > maxHealth){
+			currentHealth = maxHealth;
+		}
+	}
+
 }
