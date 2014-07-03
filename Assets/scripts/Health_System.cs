59c0e55a0c3bbaee119f47dc7776a4a6d74b2ab7
Roxanne Lopez van Dooren <roxannelopez6@hotmail.com>
2014-07-03 16:55:31 +0200

    Mejorado script y añadido manejo de armadura.

--- Assets/scripts/Health_System.cs
+++ Assets/scripts/Health_System.cs
@@ -7,24 +7,37 @@ using System.Collections;
 public class Health_System : MonoBehaviour {
 
 	public float maxHealth = 100;
-	private float currentHealth = 100;
-	public float armor = 20;
-
+	private float currentHealth;
+	public float[] armor;
+	//0 indica la armadura como una suma a la vida, 1 indica la armadura como un porcentaje sobre la vida
+	public int addedArmor = 0;
+	private float totalArmor = 0;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		currentHealth = maxHealth;
+		foreach(float f in armor){
+			totalArmor += f;
+		}
+		if(addedArmor == 0){
+			currentHealth += totalArmor;
+		}
+		else if(addedArmor == 1){
+			currentHealth += (totalArmor*currentHealth)/100;
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {	
+	void Update () {
 
-	
+		print (currentHealth);
 	}
 
 	public void dealDamage(float dmg){
+		print ("Armadura: "+totalArmor);
 		currentHealth -= dmg;
+
 		if(currentHealth <= 0){
 			currentHealth = 0;
 			gameObject.SendMessage("die");
@@ -33,7 +46,7 @@ public class Health_System : MonoBehaviour {
 
 	public void heal(float hth){
 		currentHealth += hth;
-		if(currentHealth > maxHealth){
+		if(currentHealth >= maxHealth){
 			currentHealth = maxHealth;
 		}
 	}
